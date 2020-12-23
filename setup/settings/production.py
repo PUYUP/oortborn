5416ce42db2fd30d1eccaae4becd6fd312bc70a9
PUYUP <hellopuyup@gmail.com>
2020-12-23 12:49:32 +0700

    Test csrf

--- setup/settings/production.py
+++ setup/settings/production.py
@@ -45,13 +45,13 @@ X_FRAME_OPTIONS = 'DENY'
 # Django csrf
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/2.2/ref/csrf/
-CSRF_COOKIE_DOMAIN = 'api.daftarbelanja.com'
+CSRF_COOKIE_DOMAIN = '.daftarbelanja.com'
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
 CSRF_COOKIE_SECURE = True
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_HTTPONLY = False
 CSRF_TRUSTED_ORIGINS = [
-    'api.daftarbelanja.com'
+    '.daftarbelanja.com'
 ]
 
 
